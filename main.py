20b4e77ba66418c19e4f80fd4318098134c748c1
newsshplus <60479267+newsshplus@users.noreply.github.com>
2025-09-28 13:40:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,79 @@
 # main.py
 import time
+import os
 import pandas as pd
 from typing import List, Dict
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 # Token do Browserless
 REMOTE_URL = "https://1RxfKOgd5lGGE5Ref3fee0629762ffc5a7977148d5ae"
 
-def search_products(keywords: str, max_results: int = 20, country: str = "br", language: str = "pt") -> pd.DataFrame:
+def search_products(keywords: str, max_results: int = 50, country: str = "br", language: str = "pt") -> pd.DataFrame:
     results: List[Dict] = []
     query = keywords.replace(" ", "+")
     url = f"https://www.google.com/search?tbm=shop&q={query}&hl={language}&gl={country}"
 
-    # Configurações do Chrome remoto
+    # Chrome options
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
-    # Passando capabilities corretamente
     driver = webdriver.Remote(
         command_executor=REMOTE_URL,
-        desired_capabilities=options.to_capabilities()
+        options=options
     )
 
     driver.get(url)
-    time.sleep(3)  # espera a página carregar
+    time.sleep(3)
 
-    items = driver.find_elements("css selector", "div.sh-dgr__grid-result")[:max_results]
+    # Paginação automática simples: rolar a página para carregar mais resultados
+    scroll_pause = 2
+    total_items = 0
+    while total_items < max_results:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(scroll_pause)
+        total_items = len(driver.find_elements(By.CSS_SELECTOR, "div.sh-dgr__grid-result"))
+        if total_items >= max_results:
+            break
+
+    items = driver.find_elements(By.CSS_SELECTOR, "div.sh-dgr__grid-result")[:max_results]
 
     for item in items:
         try:
-            title = item.find_element("css selector", ".Xjkr3b").text
+            title = item.find_element(By.CSS_SELECTOR, ".Xjkr3b").text
         except:
             title = None
         try:
-            price = item.find_element("css selector", ".a8Pemb").text
+            price = item.find_element(By.CSS_SELECTOR, ".a8Pemb").text
         except:
             price = None
         try:
-            link = item.find_element("tag name", "a").get_attribute("href")
+            link = item.find_element(By.TAG_NAME, "a").get_attribute("href")
         except:
             link = None
         try:
-            image = item.find_element("tag name", "img").get_attribute("src")
+            image = item.find_element(By.TAG_NAME, "img").get_attribute("src")
         except:
             image = None
+        try:
+            store = item.find_element(By.CSS_SELECTOR, ".aULzUe").text
+        except:
+            store = None
+        try:
+            rating = item.find_element(By.CSS_SELECTOR, ".Rsc7Yb").text
+        except:
+            rating = None
 
         results.append({
             "title": title,
             "price": price,
-            "store": None,
+            "store": store,
+            "rating": rating,
             "product_url": link,
             "image_url": image,
             "source": "Google Shopping",
